0e5700b2645b6654336ca67385b68e89270902b5
Tobias Frenger <37848545+Tobias-Frenger@users.noreply.github.com>
2020-02-10 11:09:20 +0100

    Update datasetTutorial.py

--- datasetTutorial.py
+++ datasetTutorial.py
@@ -1,63 +1,83 @@
-#######################################
-#   @Original_author: sentdex: https://youtu.be/j-3vuBynnOE
-#   @Edited_by: Tobias
-#######################################
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  8 01:23:54 2020
 
+@author: Tobias
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import cv2
 
+# SETTING UP TRAINING AND TEST SETS #############################################################
 DATADIR = "C:/Users/You/Your/Path/To/Dataset"
 CATEGORIES = ["Class1", "Class2", "Class3"]
 
 for category in CATEGORIES:
-    path = os.path.join(DATADIR, category) # path to img class folder
+    path = os.path.join(DATADIR, category) # path to johan and tobias voices
     for img in os.listdir(path):
         img_array = cv2.imread(os.path.join(path,img))
         img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
 
 IMG_SIZE = 128
-#plt.imshow(img_array)
+#plt.imshow(new_arr)
 #plt.show()
 
 training_data = []
-def create_training_data():
+test_data = []
+def create_training_and_test_data():
+    count = 1
+    ten = 10
     for category in CATEGORIES:
-        path = os.path.join(DATADIR, category) # path to img class folder
+        path = os.path.join(DATADIR, category) # path to johan and tobias voices
         class_num = CATEGORIES.index(category)
         for img in os.listdir(path):
             try:
                 img_array = cv2.imread(os.path.join(path,img))
                 img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
                 new_arr = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-                training_data.append([new_arr, class_num])
+                if (np.equal(count % ten, 0)):
+                    test_data.append([new_arr, class_num])
+                else:
+                    training_data.append([new_arr, class_num])
+                count += 1
             except Exception as e:
                 pass
             
-create_training_data()
+create_training_and_test_data()
 print(len(training_data))
+print(len(test_data))
 
 #RANDOMIZE TRAINING DATA
 import random
 random.shuffle(training_data)
-
+random.shuffle(test_data)
 #PRINT 20 SAMPLE TO SEE THE RESULT
 for sample in training_data[:20]:
     print(sample[1])
     
-X = []
-y = []
+train_x = []
+train_y = []
+
+for sample in test_data[:20]:
+    print(sample[1])
+    
+test_x = []
+test_y = []
 
 #X GETS THE MATRIX VALUES, y GETS THE LABELS
 for features, label in training_data:
-    X.append(features)
-    y.append(label)
+    train_x.append(features)
+    train_y.append(label)
 
+for features, label in test_data:
+    test_x.append(features)
+    test_y.append(label)
+    
+train_x = np.array(train_x).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+test_x = np.array(test_x).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
 
-X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
-
-#y = np.array(y).reshape(-1,len(y))
+# SAVING TRAIN AND TEST DATASETS #############################################################
 import pickle
 # SAVE train_x, train_y
 pickle_out = open("C:/Users/You/Your/Save/Location/training_set_X.pickle", "wb")
@@ -67,6 +87,7 @@ pickle_out = open("C:/Users/You/Your/Save/Location/training_set_y.pickle", "wb")
 pickle.dump(y, pickle_out)
 pickle_out.close()
 
+# LOADING EXAMPLE #############################################################################
 # LOAD train_x, train_y
 pickle_in = open("C:/Users/You/Your/Save/Location/training_set_X.pickle", "rb")
 X = pickle.load(pickle_in)
