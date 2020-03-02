b491941c318551a1cf9b630844f9147f49d236b8
Tobias Frenger <37848545+Tobias-Frenger@users.noreply.github.com>
2020-03-02 16:13:24 +0100

    Update mini_inception_resnet.py

--- tmp/mini_inception_resnet.py
+++ tmp/mini_inception_resnet.py
@@ -17,30 +17,37 @@ def maxPool_layer(X, pool_size, strides):
     X = MaxPooling2D(pool_size = pool_size, strides = strides, padding = "valid")(X)
     return X
 
-def inception_residual_block(X, filters):
-    shortcut = X
-    split = int(filters/2)
+def inception_residual_block(X, filters, sc = False):
+    if sc==True: shortcut = X
     
-    l1 = Conv2D(split, 1, 1, padding='same')(X)
+    l1 = Conv2D(16, 1, 1, padding='same')(X)
     l1 = common_layers(l1)
-    l1 = Conv2D(split, 5, 1, padding = 'same')(l1)
+    l1 = Conv2D(32, 5, 1, padding = 'same')(l1)
     l1 = common_layers(l1)
     
-    l2 = Conv2D(split, 1, 1, padding='same')(X)
+    l2 = Conv2D(16, 1, 1, padding='same')(X)
     l2 = common_layers(l2)
-    l2 = Conv2D(split, 3, 1, padding = 'same')(l2)
+    l2 = Conv2D(32, 3, 1, padding = 'same')(l2)
     l2 = common_layers(l2)
     
-    X = tf.keras.layers.concatenate([l1,l2])
-    X = Add()([shortcut, X])
+    l3 = Conv2D(32, 1, 1, padding='same')(X)
+    l3 = common_layers(l3)
+    
+    l4 = maxPool_layer(X, (3,3), 3)
+    l4 = Conv2D(32, 1, 1, padding='same')(X)
+    l4 = common_layers(l4)
+    
+    X = tf.keras.layers.concatenate([l1,l2,l3,l4])
+    if sc==True: X = Add()([shortcut, X])
     return X
 
 def init_model(in_dim, classes):
     inputs = tf.keras.Input(shape=in_dim)
-    X = Conv2D(64, 3, 1, padding='same')(inputs)
-    X = common_layers(X)
+    X = inception_residual_block(inputs, 128)
     X = maxPool_layer(X, 3, 3)
-    X = inception_residual_block(X, 64)
+    X = inception_residual_block(X, 128, True)
+    X = maxPool_layer(X, 3, 3)
+    X = inception_residual_block(X, 128, True)
     X = AveragePooling2D((3,3), 2)(X)
     X = Flatten()(X)
     X = Dense(classes, name="dense_out")(X)
@@ -51,4 +58,3 @@ def init_model(in_dim, classes):
 
 model = init_model((128,128,3),5)
 model.summary()
-tf.keras.utils.plot_model(model, model.name + '_architecture.jpg', show_shapes=True) # SET TO CORRECT PATH
